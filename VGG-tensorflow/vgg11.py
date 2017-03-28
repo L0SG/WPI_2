f11f7df37477296337e2c0b109abfe7559f7761a
L0SG <tkdrlf9202@gmail.com>
2017-03-28 16:05:35 +0900

    init vgg11 class

--- VGG-tensorflow/vgg11.py
+++ VGG-tensorflow/vgg11.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
-class vgg11()
+class vgg11:
+    """
+    the standard vgg11 model
+    should contain images, labels, weights and session
+    """
+    def __init__(self, images, labels, weights=None, sess=None):
