3357ba32c3a825121b37875b252f895bc878a45b
L0SG <tkdrlf9202@gmail.com>
2017-03-28 18:08:07 +0900

    initial  skeleton

---
+++ VGG-tensorflow/cifar100_utils.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+def load_data():
+    """
+    load cifar-100 dataset
+    :return: (train_x, train_y), (test_x, test_y) in numpy format
+    """
+    # implement here
+    return (train_x, train_y), (test_x, test_y)
