bd45e8e8fbbcc4b73506a2e72b07700e477f223f
Uiwon Hwang <shinyflight@gmail.com>
2017-04-06 19:17:39 +0900

    in progress 3
    3

--- VGG-tensorflow/main.py
+++ VGG-tensorflow/main.py
@@ -16,11 +16,11 @@ with tf.Session(config=config) as sess:
     vgg_model = vgg11(weights=None, sess=sess, shape_x = shape_x, shape_y = shape_y)
     # train the model
     vgg_model.train(images=train_x, labels=train_y,
-                    epochs=10, val_split=0.1, save_weights=True)
+                    epochs=10, val_split=0.005, save_weights=True)
     # predict labels for the test images
-    preds = vgg_model.predict(images=None)
+    #preds = vgg_model.predict(images=None)
 
     # calculate accuracy
-    accuracy = np.sum([preds[i] == test_y[i] for i in xrange(test_y.shape[0])])
-    accuracy /= test_y.shape[0]
-    print('test accuracy: ' + str(accuracy))
+    #accuracy = np.sum([preds[i] == test_y[i] for i in xrange(test_y.shape[0])])
+    #accuracy /= test_y.shape[0]
+    #print('test accuracy: ' + str(accuracy))
